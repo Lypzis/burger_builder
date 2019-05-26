6661c4bf175f08a9980782cbae6f436bb04c24ef
Lypzis <victorvp2008@hotmail.com>
2019-05-26 10:19:59 -0300

    Update Burger.js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -8,14 +8,20 @@ import Aux from '../../hoc/Aux';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = props => {
-    const transformedIngredients = Object.keys(props.ingredients).map(ingKey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(ingKey => {
         // returns an array of the respective ingredient value.
         // the "Array()" method will create something like [cheese, cheese], 
         // which is the given number value for that ingredient
         return [...Array(props.ingredients[ingKey])].map((_, index) =>  // then, maps them/it into components 
             <BurgerIngredient key={ingKey + index} type={ingKey} />
-        );
-    }); // "Object.keys" turns an object's keys into an array
+        );// "Object.keys" turns an object's keys into an array
+    }).reduce((arr, el) => { // reduce gets each element of the array, parameters are the accumulator and current element 
+        return arr.concat(el);
+    }, []); // initial value
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please, add some ingredients! :D</p>
+    };
 
     return (
         <Aux>
