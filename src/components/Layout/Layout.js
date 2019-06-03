1f364894135d99a4334798b20a441e3dfd7d5bd6
Lypzis <victorvp2008@hotmail.com>
2019-06-03 11:31:09 -0300

    Adding SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,11 +5,13 @@ import classes from './Layout.css';
 import Aux from '../../hoc/Aux';
 import withClass from '../../hoc/withClass';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = props => {
     return (
         <Aux>
             <Toolbar />
+            <SideDrawer />
             <header><p>SideDrawer</p></header>
             <main className={classes.content}>{props.children}</main>
             <footer>Burger Builder © 2019 by Lypzis.</footer>
