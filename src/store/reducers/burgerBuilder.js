87935d2ae2ef24a562c29e4d3005026f8ec32820
Victor V. Piccoli <victorvp2008@hotmail.com>
2019-11-09 09:20:48 -0300

    Update reducers

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -15,50 +15,66 @@ const INGREDIENT_PRICES = {
     bacon: .8
 };
 
+const addIngredient = (state, action) => {
+    // key: increment value of respective key, which is the ingrediente I want to add
+    const updatedIngredient = { [action.ingredientName]: ++state.ingredients[action.ingredientName] };
+
+    // updatedIngredient is set into 'state.ingredients' where it respectively replaces the old value
+    const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+
+    // then the new ingredients and total price are passed into and object
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    }
+
+    // which returns the utility function again, passing where the old state values
+    // are joined with the updated values, respectively: ingredients and totalPrice.
+    return updateObject(state, updatedState);
+}
+
+const removeIngredient = (state, action) => {
+    const updatedIngredient2 = { [action.ingredientName]: --state.ingredients[action.ingredientName] };
+    const updatedIngredients2 = updateObject(state.ingredients, updatedIngredient2);
+    const updatedState2 = {
+        ingredients: updatedIngredients2,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    }
+
+    return updateObject(state, updatedState2); // '2' because you can't reassign a const, this obviously not ideal
+}
+
+const setIngredients = (state, action) => {
+    return updateObject(
+        state,
+        {
+            ingredients: { // turned into an object for manualy setting the position of ingredients.
+                salad: action.ingredients.salad,
+                bacon: action.ingredients.bacon,
+                cheese: action.ingredients.cheese,
+                meat: action.ingredients.meat // flexibility is lost this way... D: 
+            },
+            totalPrice: 4, // this will reset the price, when back to BurgerBuilder container
+            error: false
+        });
+}
+
+const fetchIngredientsFailed = (state, action) => {
+    return updateObject(state, { error: true });
+}
+
+// now the reducer is very small :D, making it easier to read
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_INGREDIENT:
-            // key: increment value of respective key, which is the ingrediente I want to add
-            const updatedIngredient = { [action.ingredientName]: ++state.ingredients[action.ingredientName] };
-
-            // updatedIngredient is set into 'state.ingredients' where it respectively replaces the old value
-            const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
-
-            // then the new ingredients and total price are passed into and object
-            const updatedState = {
-                ingredients: updatedIngredients,
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            }
-
-            // which returns the utility function again, passing where the old state values
-            // are joined with the updated values, respectively: ingredients and totalPrice.
-            return updateObject(state, updatedState);
+            return addIngredient(state, action);
         case actionTypes.REMOVE_INGREDIENT:
-            const updatedIngredient2 = { [action.ingredientName]: --state.ingredients[action.ingredientName] };
-            const updatedIngredients2 = updateObject(state.ingredients, updatedIngredient2);
-            const updatedState2 = {
-                ingredients: updatedIngredients2,
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            }
-
-            return updateObject(state, updatedState2); // '2' because you can't reassign a const, this obviously not ideal
+            return removeIngredient(state, action);
         case actionTypes.SET_INGREDIENTS: // ingredients initialized
-            return updateObject(
-                state,
-                {
-                    ingredients: { // turned into an object for manualy setting the position of ingredients.
-                        salad: action.ingredients.salad,
-                        bacon: action.ingredients.bacon,
-                        cheese: action.ingredients.cheese,
-                        meat: action.ingredients.meat // flexibility is lost this way... D: 
-                    },
-                    totalPrice: 4, // this will reset the price, when back to BurgerBuilder container
-                    error: false
-                });
+            return setIngredients(state, action);
         case actionTypes.FETCH_INGREDIENTS_FAILED:
-            return updateObject(state, { error: true });
-        default:
-            return state;
+            return fetchIngredientsFailed(state, action);
+        default: return state;
     }
 }
 
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -7,30 +7,61 @@ const initialState = {
     purchased: false
 }
 
+const purchaseInit = (state, action) => {
+    return updateObject(state, { purchased: false }); // resets when going to checkout 
+}
+
+const purchaseBurgerStart = (state, action) => {
+    return updateObject(state, { loading: true }); // loading complete
+}
+
+const purchaseBurgerSuccess = (state, action) => {
+    const newOrder = updateObject(action.orderData, { id: action.orderId });
+    return updateObject(state, {
+        loading: false,
+        orders: state.orders.concat(newOrder), // remember with concat the array is added immutably, creating a new one :D
+        purchased: true
+    });
+}
+
+const purchaseBurgerFail = (state, action) => {
+    return updateObject(state, { loading: false });
+}
+
+const fetchOrdersStart = (state, action) => {
+    return updateObject(state, { loading: true });
+}
+
+const fetchOrdersSuccess = (state, action) => {
+    return updateObject(state, {
+        orders: action.orders, // as is set in fetchedOrderSuccess in actions/order.js
+        loading: false // it is not loading anylonger at this point
+    });
+}
+
+const fetchOrdersFail = (state, action) => {
+    return updateObject(state, { loading: false });
+}
+
+// ADD a delete function here, as a challenge
+
+// now the reducer is very small :D, making it easier to read
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.PURCHASE_INIT:
-            return updateObject(state, { purchased: false }); // resets when going to checkout 
+            return purchaseInit(state, action);
         case actionTypes.PURCHASE_BURGER_START:
-            return updateObject(state, { loading: true }); // loading complete
+            return purchaseBurgerStart(state, action);
         case actionTypes.PURCHASE_BURGER_SUCCESS:
-            const newOrder = updateObject(action.orderData, { id: action.orderId });
-            return updateObject(state, {
-                loading: false,
-                orders: state.orders.concat(newOrder), // remember with concat the array is added immutably, creating a new one :D
-                purchased: true
-            });
+            return purchaseBurgerSuccess(state, action);
         case actionTypes.PURCHASE_BURGER_FAIL:
-            return updateObject(state, { loading: false });
+            return purchaseBurgerFail(state, action)
         case actionTypes.FETCH_ORDERS_START:
-            return updateObject(state, { loading: true });
+            return fetchOrdersStart(state, action);
         case actionTypes.FETCH_ORDERS_SUCCESS:
-            return updateObject(state, {
-                orders: action.orders, // as is set in fetchedOrderSuccess in actions/order.js
-                loading: false // it is not loading anylonger at this point
-            });
+            return fetchOrdersSuccess(state, action);
         case actionTypes.FETCH_ORDERS_FAIL:
-            return updateObject(state, { loading: false });
+            return fetchOrdersFail(state, action);
         default:
             return state;
     }
